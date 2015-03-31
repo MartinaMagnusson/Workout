7dac783e977d8e146d3f6e6c81b81d2a18838206
roje <roje.haddad.win14@utb.hermods.se>
2015-03-31 14:05:54 +0200

    Added classes(DAL,User,WorkoutContext,WorkoutContextInitializer,Global)

---
+++ WorkoutLabb/WorkoutLabb/Models/WorkoutContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WorkoutLabb.Models
+{
+    public class WorkoutContext: DbContext
+    {
+    }
+}
